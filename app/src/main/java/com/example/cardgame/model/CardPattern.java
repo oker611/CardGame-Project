f345a13f6d385d42f2ffa5ca1a2bb782e1243fe1
Fu-junye <fujunye11@gmail.com>
2026-04-16 14:19:21 +0800

    changes

---
+++ app/src/main/java/com/example/cardgame/model/CardPattern.java
@@ -0,0 +1,13 @@
+package com.example.cardgame.model;
+
+public enum CardPattern {
+    INVALID,
+    SINGLE,
+    PAIR,
+    TRIPLE,
+    STRAIGHT,
+    FLUSH,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    STRAIGHT_FLUSH
+}
